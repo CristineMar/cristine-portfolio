abe8ea830aedb6a3cdc71163dceaea1c2e579e05
Cristine Paula Marquez Santiago <cmarquezs2021@cepnet.net>
2022-05-24 15:24:52 +0200

    fixed photo project

--- src/info.js
+++ src/info.js
@@ -15,36 +15,6 @@ let info = {
     github: "https://github.com/CristineMar",
     jsfiddle: "https://jsfiddle.net/user/fiddles/all/",
   },
-  education: [
-    {
-      name: "",
-      course: "",
-      place: "",
-      date: "",
-      degree: "",
-      gpa: "",
-      description: "",
-      skills: ["HTML5", "CSS3", "JavaScript", "PHP", "MySql", "Laravel"],
-    },
-  ],
-  experience: [
-    {
-      name: "",
-      place: "",
-      date: "",
-      position: "Full Stack Web Developer",
-      description:
-        "Developing Front-end website architecture.Designing user interactions on web pages.Ensuring responsiveness of applications.Working alongside graphic designers for web design features.Seeing through a project from conception to finished product.Staying abreast of developments in web applications and programming languages. Degree: Program Help Cod COR 351201.Strong organizational and project management skills.Proficiency with fundamental Front-end languages such as HTML, CSS, and JavaScript.Familiarity with JavaScript frameworks such as  React.js, and Vue.js.",
-      skills: [
-        "HTML5",
-        "CSS3",
-        "JavaScript",
-        "Bootstrap",
-        "React.js",
-        "Vue.js",
-      ],
-    },
-  ],
   skills: [
     {
       title: "languages",
@@ -84,46 +54,6 @@ let info = {
       visit: "https://natainbogdan.netlify.app/",
       description:
         "Todo Application was made to help me with the learning schedule and it seams was doing a great job this app helping me to organize all the daily tasks.",
-    },
-
-    {
-      name: "Grocery-listing-react",
-      pictures: [
-        // {
-        //   img: require("./src/assets/portfolio/clumpr/logo.png")
-        // },
-        /* {
-            img: require("./src/assets/portfolio/grocery/one.png")
-          },
-          {
-            img: require("./src/assets/portfolio/grocery/two.png")
-          } */
-      ],
-      technologies: ["CSS", "JavaScript", "React.js", "HTML5"],
-      category: "Web App",
-      github: "https://github.com/Natain/grocery-listing-react",
-      date: "",
-      visit: "https://grocery-listing-react.netlify.app/",
-      description:
-        "Grocery-listing-react it's a simple app that can help you doing shoppings.",
-    },
-  ],
-  recommendations: [
-    {
-      title:
-        "In his internship, Bogdan Natain has demonstrated excellent learning ability, and with a dedicated, task oriented approach, he was able to complete his assignment ahead of time.",
-      author: "Timothy Gray",
-      position: "CTO",
-      company: "Startup Norway As",
-      location: " Oslo, Norway",
-    },
-    {
-      title:
-        "I feel his budding leadership abilities will become even more effective in a diverse and challenging environment.",
-      author: "Hansley Jacob",
-      position: "Project Manager",
-      company: "Startup Norway As",
-      location: "Oslo, Norway",
     },
   ],
 };
