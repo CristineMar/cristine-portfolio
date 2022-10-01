99692411d90293910e52507851a3cbed68348072
Cristine Marquez <princess28cristine@gmail.com>
2022-10-01 20:14:39 +0200

    s

--- src/components/About.vue
+++ src/components/About.vue
@@ -126,8 +126,8 @@
              <a
               type="button"
               class="btn btn-primary border-btn"
-              href="public/docs/Cristine_Marquez_CV_ENG.pdf"
-              ttarget="_blank"
+              href="../public/docs/Cristine_Marquez_CV_ENG.pdf"
+              download
               >Download</a >
             </h5>
             <button
